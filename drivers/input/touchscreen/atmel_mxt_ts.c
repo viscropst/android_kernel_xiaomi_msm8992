4758207fc62addd36c3da4a5943abd11294cff9a
Ketut Putu Kumajaya <ketut.kumajaya@gmail.com>
2016-03-19 19:24:49 +0700

    atmel_mxt_ts: Restore The Linux Foundation copyright
    Change-Id: I9d3cdbe86e9be68bf97b8af56f9c2bb59c49004d
    Signed-off-by: Ketut Putu Kumajaya <ketut.kumajaya@gmail.com>

--- drivers/input/touchscreen/atmel_mxt_ts.c
+++ drivers/input/touchscreen/atmel_mxt_ts.c
@@ -5,6 +5,7 @@
  * Copyright (C) 2011 Atmel Corporation
  * Copyright (C) 2016 XiaoMi, Inc.
  * Author: Joonyoung Shim <jy0922.shim@samsung.com>
+ * Copyright (c) 2011-2014, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute  it and/or modify it
  * under  the terms of  the GNU General  Public License as published by the
