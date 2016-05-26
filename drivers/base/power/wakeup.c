d6aee0256282d89f9bc73a70d0317540313040a6
AndropaX <kpax.mar@gmail.com>
2016-05-26 10:02:07 +0200

    Resurrected Kernel v1.02

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -16,8 +16,15 @@
 #include <linux/debugfs.h>
 #include <linux/types.h>
 #include <trace/events/power.h>
+#include <linux/moduleparam.h>
 
 #include "power.h"
+
+static bool enable_bluedroid_timer_ws = true;
+module_param(enable_bluedroid_timer_ws, bool, 0644);
+
+static bool enable_ipa_ws = true;
+module_param(enable_ipa_ws, bool, 0644);
 
 /*
  * If set, the suspend/hibernate code will abort transitions to a sleep state
@@ -428,6 +435,10 @@ static void wakeup_source_activate(struct wakeup_source *ws)
 {
 	unsigned int cec;
 
+        if (!enable_bluedroid_timer_ws && !strcmp(ws->name, "bluedroid_timer"))
+		return;
+        if (!enable_ipa_ws && !strcmp(ws->name, "IPA_WS"))
+		return;
 	if (WARN(wakeup_source_not_registered(ws),
 			"unregistered wakeup source\n"))
 		return;
