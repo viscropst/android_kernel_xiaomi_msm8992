6e93fdf081bda556a7d164a49e2de4812e0b4620
AndropaX <kpax.mar@gmail.com>
2016-07-31 17:05:42 +0200

    merge damn f10cb84585ceb412e7909933c622bc02388e7433

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -152,6 +152,10 @@ struct cpufreq_interactive_tunables {
 	 * frequency.
 	 */
 	unsigned int max_freq_hysteresis;
+
+        /* Whether to change frequency immediately for notification */
+ 	bool fast_ramp_up;
+	bool fast_ramp_down;
 };
 
 /* For cases where we have single governor instance for system */
@@ -494,7 +498,7 @@ static void __cpufreq_interactive_timer(unsigned long data, bool is_notif)
 
 	if (cpu_load >= tunables->go_hispeed_load || tunables->boosted) {
 		if (ppol->target_freq < tunables->hispeed_freq &&
-		    cpu_load <= MAX_LOCAL_LOAD) {
+		    (!tunables->fast_ramp_up || cpu_load <= MAX_LOCAL_LOAD)) {
 			new_freq = tunables->hispeed_freq;
 		} else {
 			new_freq = choose_freq(ppol, loadadjfreq);
@@ -506,7 +510,7 @@ static void __cpufreq_interactive_timer(unsigned long data, bool is_notif)
 		new_freq = choose_freq(ppol, loadadjfreq);
 	}
 
-	if (cpu_load <= MAX_LOCAL_LOAD &&
+	if ((!tunables->fast_ramp_up || cpu_load <= MAX_LOCAL_LOAD) &&
 	    ppol->target_freq >= tunables->hispeed_freq &&
 	    new_freq > ppol->target_freq &&
 	    now - ppol->hispeed_validate_time <
@@ -529,7 +533,8 @@ static void __cpufreq_interactive_timer(unsigned long data, bool is_notif)
 
 	new_freq = ppol->freq_table[index].frequency;
 
-	if (!is_notif && new_freq < pcpu->target_freq &&
+	if ((!tunables->fast_ramp_down || !is_notif) &&
+	    new_freq < pcpu->target_freq &&
 	    now - pcpu->max_freq_hyst_start_time <
 	    tunables->max_freq_hysteresis) {
 		trace_cpufreq_interactive_notyet(max_cpu, cpu_load,
@@ -543,7 +548,8 @@ static void __cpufreq_interactive_timer(unsigned long data, bool is_notif)
 	 * floor frequency for the minimum sample time since last validated.
 	 */
 	max_fvtime = max(pcpu->floor_validate_time, pcpu->local_fvtime);
-	if (!is_notif && new_freq < pcpu->floor_freq &&
+	if ((!tunables->fast_ramp_down || !is_notif) &&
+	    new_freq < pcpu->floor_freq &&
 	    pcpu->target_freq >= pcpu->policy->cur) {
 		if (now - max_fvtime < tunables->min_sample_time) {
 			trace_cpufreq_interactive_notyet(
@@ -940,6 +946,8 @@ static ssize_t store_##file_name(					\
 }
 show_store_one(max_freq_hysteresis);
 show_store_one(align_windows);
+show_store_one(fast_ramp_up);
+show_store_one(fast_ramp_down);
 
 static ssize_t show_go_hispeed_load(struct cpufreq_interactive_tunables
 		*tunables, char *buf)
@@ -1351,6 +1359,8 @@ show_store_gov_pol_sys(use_sched_load);
 show_store_gov_pol_sys(use_migration_notif);
 show_store_gov_pol_sys(max_freq_hysteresis);
 show_store_gov_pol_sys(align_windows);
+show_store_gov_pol_sys(fast_ramp_up);
+show_store_gov_pol_sys(fast_ramp_down);
 show_store_gov_pol_sys(screen_off_maxfreq);
 
 #define gov_sys_attr_rw(_name)						\
@@ -1379,6 +1389,8 @@ gov_sys_pol_attr_rw(use_sched_load);
 gov_sys_pol_attr_rw(use_migration_notif);
 gov_sys_pol_attr_rw(max_freq_hysteresis);
 gov_sys_pol_attr_rw(align_windows);
+gov_sys_pol_attr_rw(fast_ramp_up);
+gov_sys_pol_attr_rw(fast_ramp_down);
 gov_sys_pol_attr_rw(screen_off_maxfreq);
 
 static struct global_attr boostpulse_gov_sys =
@@ -1404,6 +1416,8 @@ static struct attribute *interactive_attributes_gov_sys[] = {
 	&use_migration_notif_gov_sys.attr,
 	&max_freq_hysteresis_gov_sys.attr,
 	&align_windows_gov_sys.attr,
+        &fast_ramp_up_gov_sys.attr,
+ 	&fast_ramp_down_gov_sys.attr,
 	&screen_off_maxfreq_gov_sys.attr,
 	NULL,
 };
@@ -1430,6 +1444,8 @@ static struct attribute *interactive_attributes_gov_pol[] = {
 	&use_migration_notif_gov_pol.attr,
 	&max_freq_hysteresis_gov_pol.attr,
 	&align_windows_gov_pol.attr,
+        &fast_ramp_up_gov_pol.attr,
+ 	&fast_ramp_down_gov_pol.attr,
 	&screen_off_maxfreq_gov_pol.attr,
 	NULL,
 };
