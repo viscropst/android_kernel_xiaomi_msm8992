b44ce84bacebe8d27c1d30ea22410aea8ef7c6b0
AndropaX <kpax.mar@gmail.com>
2016-12-04 22:03:08 +0100

    merge typo
    Change-Id: I62d4106eb5e71ad70024bc89e0b13cce827b7e92

--- drivers/input/touchscreen/synaptics_dsx/synaptics_dsx_fw_update.c
+++ drivers/input/touchscreen/synaptics_dsx/synaptics_dsx_fw_update.c
@@ -25,7 +25,7 @@
 #include <linux/input.h>
 #include <linux/firmware.h>
 #include <linux/platform_device.h>
-#include <linux/input/synaptics_dsx_xiaomi.h>
+#include <linux/input/synaptics_dsx.h>
 #include "synaptics_dsx_core.h"
 
 #define FW_IMAGE_NAME "synaptics/startup_fw_update.img"
